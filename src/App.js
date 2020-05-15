21ed7b6c8c67dbe2003631e4b70e335a35125044
tysker <oerteljoerg@gmail.com>
2020-05-15 08:38:29 +0200

    add new files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, {Component} from 'react';
 import P5 from 'p5';
-import { s } from "./components/Drawing";
+import styled from 'styled-components';
 import './style/App.css';
 
-/*
-Refs are created using React.createRef() and attached to React elements via the ref attribute.
-Refs are commonly assigned to an instance property when a component is constructed so they can
-be referenced throughout the component.
-*/
-class App extends Component {
-    constructor(props) {
-        super(props);
-       // this.myRef = React.createRef();
-        console.log("Hello from the constructor:  ")
+
+export default class App extends Component {
+    state = {
+        val: 111
+    }
+
+    canvas = (sketch) => {
+        let button;
+        let slider;
+        sketch.setup = () => {
+            sketch.createCanvas(600, 600);
+            sketch.background(111);
+            //
+            button = sketch.createButton("Click ME");
+            button.mousePressed(changeBG);
+            //
+            slider = sketch.createSlider(0, 255, 127);
+            slider.position(50, 50);
+            slider.style('width', '80px');
+            changeSD();
+        }
+
+        const changeSD = () => {
+            //let val = slider.value();
+            this.setState({val: slider.value()})
+            sketch.background(this.state.val);
+        }
+
+        const changeBG = () => {
+            sketch.background(this.state.val);
+            this.setState({val: sketch.random(255)});
+            console.log(this.state.val)
+        }
+
+        sketch.draw = () => {
+            //changeSD();
+        }
     }
 
     componentDidMount() {
-        //this.myP5 = new P5(s, this.myRef.current);
-        let myp5 = new P5(s, document.getElementById('p5sketch'))
-        console.log("Hallo from DidMount:  ")
-        console.log(myp5);
+        let myp5 = new P5(this.canvas, document.getElementById('p5sketch'))
+        console.log(myp5)
     }
 
     render() {
-        console.log("Hallo from render():  ")
         return (
-            <CanvasGrid id="p5sketch" ref={this.myRef}>
-
-            </CanvasGrid>
+            <div>
+                <CanvasGrid id="p5sketch"/>
+            </div>
         )
+
     }
 }
 
-export default App;
 
 const CanvasGrid = styled.div`
     display: grid;

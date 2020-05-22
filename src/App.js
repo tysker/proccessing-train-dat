8378e2e4269633040a7d419ac5fe25e5537d0754
tysker <oerteljoerg@gmail.com>
2020-05-22 15:30:06 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import P5 from 'p5';
 import styled from 'styled-components';
 import './style/App.css';
@@ -10,6 +10,7 @@ const railTrack = {
     x: 200,
     y: 200
 }
+let button;
 let trackMap = [];
 trackMap.push(railTrack);
 let sideX = 10;
@@ -17,10 +18,11 @@ let sideY = 20;
 let lastRailInTrackMap = trackMap.length - 1;
 let newX = 0;
 let newY = 0;
+
 class App extends Component {
 
     addStraightTrack = (event) => {
-        switch(trackMap[lastRailInTrackMap].direction){
+        switch (trackMap[lastRailInTrackMap].direction) {
             case 90:
                 newX = trackMap[lastRailInTrackMap].x + 20;
                 newY = trackMap[lastRailInTrackMap].y;
@@ -29,17 +31,17 @@ class App extends Component {
 
             case 180:
                 newX = trackMap[lastRailInTrackMap].x;
-                newY = trackMap[lastRailInTrackMap].y+20;
+                newY = trackMap[lastRailInTrackMap].y + 20;
                 break;
 
             case 270:
-                newX = trackMap[lastRailInTrackMap].x-20;
+                newX = trackMap[lastRailInTrackMap].x - 20;
                 newY = trackMap[lastRailInTrackMap].y;
                 break;
 
             case 360:
                 newX = trackMap[lastRailInTrackMap].x;
-                newY = trackMap[lastRailInTrackMap].y-20;
+                newY = trackMap[lastRailInTrackMap].y - 20;
                 break;
         }
         railTrack.x = newX;
@@ -79,38 +81,46 @@ class App extends Component {
         sketch.rect(trackMap[i].x, trackMap[i].y, sideX, sideY);
     }
 
-    rotateTrack = (sketch, direction) =>{
-        sketch.rotate(3.14/direction);
-   }
+    rotateTrack = (sketch, direction) => {
+        sketch.rotate(3.14 / direction);
+    }
     clicked = (sketch) => {
         const d = sketch.dist(sketch.mouseX, sketch.mouseY, 10, 100);
         if (d < 50) {
             sketch.fill(200);
         }
     }
+
+    rotate = (i) => {
+        //sketch.rotateZ(90);
+        trackMap[0].direction += 22.5;
+        console.log(trackMap.map(m => `${m.direction} ${m.id}`))
+        console.log(trackMap[0].direction);
+    }
     canvas = (sketch) => {
 
         sketch.setup = () => {
+
             sketch.createCanvas(1500, 600);
             sketch.background(111);
+            button = sketch.createButton("Rotate")
+            button.mousePressed(this.rotate);
+
         }
+
+
 
         sketch.mousePressed = (event) => {
             this.clicked(sketch);
         }
 
+
         sketch.draw = () => {
             for (let i = 0; i < trackMap.length; i++) {
                 switch (trackMap[i].trackType) {
                     case 'Straight':
-                        if(trackMap[i].id === 1 ){
-                            this.rotateTrack(sketch,trackMap[i].direction);
-                            console.log(i)
-                            console.log(trackMap[i].direction)
-                        }else if(trackMap[i].direction !== trackMap[i-1].direction ){
-                            this.rotateTrack(sketch,trackMap[i].direction);
-                        }
-                        this.straightTrack(sketch,i);
+
+                        this.straightTrack(sketch, i);
                         break;
 
                     case 'Curve':
@@ -123,27 +133,27 @@ class App extends Component {
 
                 }
             }
+
         }
     }
-        componentDidMount()
-        {
-            let myp5 = new P5(this.canvas, document.getElementById('p5sketch'))
-            console.log(myp5)
-        }
 
-        render()
-        {
-            return (
+    componentDidMount() {
+        let myp5 = new P5(this.canvas, document.getElementById('p5sketch'))
+        console.log(myp5)
+    }
+
+    render() {
+        return (
+            <div>
+                <CanvasGrid id="p5sketch"/>
                 <div>
-                    <CanvasGrid id="p5sketch"/>
-                    <div>
-                        <button onClick={this.addStraightTrack}>ADD Straight</button>
-                    </div>
+                    <button onClick={this.addStraightTrack}>ADD Straight</button>
                 </div>
-            )
+            </div>
+        )
 
-        }
     }
+}
 
 export default App;
 

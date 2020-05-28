cd92be599af8ea9ea40e353327467ff0fc75a1f0
MadsJuste <madsjuste89@gmail.com>
2020-05-28 08:15:47 +0200

    arbejdet på curve rotation

--- src/models/Curve.js
+++ src/models/Curve.js
@@ -48,6 +48,7 @@ export const curve = (railroadMap, trackObject, obj) => {
                     obj.curveX = xPointCurve(lastTrack.x2,20,225);
                     obj.curveY = yPointCurve(lastTrack.y2,20,225);
                     obj.direction = "west";
+                    obj.grader = 180;
                     obj.x2 = xPointCurve(obj.curveX, 20, obj.endAngle)
                     obj.y2 = yPointCurve(obj.curveY, 20, obj.endAngle)
                     console.log(obj.direction)
@@ -58,6 +59,7 @@ export const curve = (railroadMap, trackObject, obj) => {
                     obj.curveX = xPointCurve(lastTrack.x2,20,270);
                     obj.curveY = yPointCurve(lastTrack.y2,20,270);
                     obj.direction = "north-west";
+                    obj.grader = 225;
                     obj.x2 = xPointCurve(obj.curveX, 20, obj.endAngle)
                     obj.y2 = yPointCurve(obj.curveY, 20, obj.endAngle)
                     console.log(obj.direction)
@@ -68,6 +70,7 @@ export const curve = (railroadMap, trackObject, obj) => {
                     obj.curveX = xPointCurve(lastTrack.x2,20,315);
                     obj.curveY = yPointCurve(lastTrack.y2,20,315);
                     obj.direction = "north";
+                    obj.grader = 270;
                     obj.x2 = xPointCurve(obj.curveX, 20, obj.endAngle)
                     obj.y2 = yPointCurve(obj.curveY, 20, obj.endAngle)
                     console.log(obj.direction)
@@ -78,6 +81,7 @@ export const curve = (railroadMap, trackObject, obj) => {
                     obj.curveX = xPointCurve(lastTrack.x2,20,360);
                     obj.curveY = yPointCurve(lastTrack.y2,20,360);
                     obj.direction = "north-east";
+                    obj.grader = 315;
                     obj.x2 = xPointCurve(obj.curveX, 20, obj.endAngle)
                     obj.y2 = yPointCurve(obj.curveY, 20, obj.endAngle)
                     console.log(obj.direction)
@@ -88,6 +92,7 @@ export const curve = (railroadMap, trackObject, obj) => {
                     obj.curveX = xPointCurve(lastTrack.x2,20,45);
                     obj.curveY = yPointCurve(lastTrack.y2,20,45);
                     obj.direction = "east";
+                    obj.grader = 360;
                     obj.x2 = xPointCurve(obj.curveX, 20, obj.endAngle)
                     obj.y2 = yPointCurve(obj.curveY, 20, obj.endAngle)
                     console.log(obj.direction)
